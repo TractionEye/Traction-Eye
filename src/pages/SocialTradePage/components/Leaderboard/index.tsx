9ec586f279207f79857a48900eb5f265bc8a0a9e
Pavel Meyer <meyerpavel1207@gmail.com>
2025-01-28 14:51:57 +0300

    Fix trophy ui

--- src/pages/SocialTradePage/components/Leaderboard/index.tsx
+++ src/pages/SocialTradePage/components/Leaderboard/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { FixedSizeList as List } from 'react-window';
+import { FixedSizeList as List, ListChildComponentProps } from 'react-window';
 import { Box } from '@mui/material';
 import { LeaderboardCard } from './LeaderboardCard';
 import { TraderData } from './types';
@@ -20,12 +20,12 @@ const generateTestData = (count: number): TraderData[] => {
 const testData = generateTestData(100);
 
 export const Leaderboard: FC<{ handleMyStrategyClick: () => void }> = ({ handleMyStrategyClick }) => {
-  const rowRenderer = ({ index, style }: any) => {
+  const rowRenderer = ({ index, style }: ListChildComponentProps) => {
     const item = testData[index];
     
     return (
       <div style={{ ...style, padding: '0 20px 20px' }}>
-        <LeaderboardCard trader={item} handleMyStrategyClick={handleMyStrategyClick} />
+        <LeaderboardCard trader={item} handleMyStrategyClick={handleMyStrategyClick} index={index} />
       </div>
     );
   };
